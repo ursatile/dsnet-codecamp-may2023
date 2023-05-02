cda769cff485954f96f830f18db09e8e6b028d5f
Dylan Beattoie <dylan@dylanbeattie.net>
2023-05-02 13:58:31 +0100

    Pub/sub for Autobarn web API

--- dotnet/Autobarn.AuditLog/Program.cs
+++ dotnet/Autobarn.AuditLog/Program.cs
@@ -1,3 +1,4 @@
+using Autobarn.Messages;
 using EasyNetQ;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
@@ -21,18 +22,31 @@ host.Run();
 
 public class AuditLogService : IHostedService {
 	private readonly ILogger<AuditLogService> logger;
+	private readonly IBus bus;
 
-	public AuditLogService(ILogger<AuditLogService> logger) {
+	private string subscriptionId = $"autobarn.auditlog@{Environment.MachineName}";
+
+	public AuditLogService(ILogger<AuditLogService> logger, IBus bus) {
 		this.logger = logger;
+		this.bus = bus;
 	}
 
 	public Task StartAsync(CancellationToken cancellationToken) {
 		logger.LogInformation("Started AuditLogService");
-		return Task.CompletedTask;
+		return bus.PubSub.SubscribeAsync<NewVehicleListed>(
+			subscriptionId,
+			HandleNewVehicleListed
+		);
 	}
 
 	public Task StopAsync(CancellationToken cancellationToken) {
 		logger.LogInformation("Stopped AuditLogService");
 		return Task.CompletedTask;
+	}
+
+	private async Task HandleNewVehicleListed(NewVehicleListed message) {
+		logger.LogInformation($"{Thread.CurrentThread.ManagedThreadId} NewVehicleListed: {message}");
+		await Task.Delay(TimeSpan.FromSeconds(1));
+		logger.LogInformation($"{Thread.CurrentThread.ManagedThreadId} NewVehicleListed: {message}");
 	}
 }
