50fc4ac8aeb3357961f2d401f3cba2d3196cd084
Dylan Beattie <dylan@dylanbeattie.net>
2023-05-03 11:58:33 +0300

    Standalone gRPC demo in .NET

---
+++ grpc/GrpcGreeterClient/Program.cs
@@ -0,0 +1,13 @@
+﻿using Grpc.Net.Client;
+using GrpcGreeter;
+
+using var channel = GrpcChannel.ForAddress("http://localhost:5235");
+var grpcClient = new Greeter.GreeterClient(channel);
+Console.WriteLine("Ready! Press any key to send a gRPC request (or Ctrl-C to quit):");
+while (true) {
+	Console.ReadKey(true);
+	var request = new HelloRequest { Name = "CodeCamp" };
+	var reply = await grpcClient.SayHelloAsync(request);
+	Console.WriteLine("Greeting: " + reply.Message);
+}
+
