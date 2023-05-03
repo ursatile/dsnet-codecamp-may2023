e637ad6e4a8a434e3d47b8c26b8ea85a8cd93116
Dylan Beattoie <dylan@dylanbeattie.net>
2023-05-03 12:00:50 +0100

    Demo of versioning protobuf messages

--- grpc/GrpcGreeter/Services/GreeterService.cs
+++ grpc/GrpcGreeter/Services/GreeterService.cs
@@ -1,21 +1,33 @@
 using Grpc.Core;
 using GrpcGreeter;
 
-namespace GrpcGreeter.Services;
+namespace GrpcGreeter.Services {
 
-public class GreeterService : Greeter.GreeterBase
-{
-    private readonly ILogger<GreeterService> _logger;
-    public GreeterService(ILogger<GreeterService> logger)
-    {
-        _logger = logger;
-    }
+	public class GreeterService : Greeter.GreeterBase {
+		private readonly ILogger<GreeterService> _logger;
+		public GreeterService(ILogger<GreeterService> logger) {
+			_logger = logger;
+		}
 
-    public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
-    {
-        return Task.FromResult(new HelloReply
-        {
-            Message = "Hello " + request.Name
-        });
-    }
+		public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context) {
+            _logger.LogInformation("Request {request}", request.Number);
+            var name = $"{request.FirstName} {request.LastName}";
+			var message = request.LanguageCode switch {
+				"en-GB" => $"Good morning, {name}",
+				"en-US" => $"Howdy, {name}",
+				"ro-RO" => $"Buna dimineata, {name}!",
+				"ru-RU" => $"Доброе Утро, {name}",
+				"fr-FR" => $"Bonjour, {name}",
+				_ => $"Hello, {name}"
+			};
+			return Task.FromResult(new HelloReply {
+				Message = message
+			});
+		}
+	}
+}
+namespace GrpcGreeter {
+	public partial class HelloRequest {
+		public string Name => FirstName + " " + LastName;
+	}
 }
--- grpc/GrpcGreeterClient/Program.cs
+++ grpc/GrpcGreeterClient/Program.cs
@@ -4,9 +4,22 @@ using GrpcGreeter;
 using var channel = GrpcChannel.ForAddress("http://localhost:5235");
 var grpcClient = new Greeter.GreeterClient(channel);
 Console.WriteLine("Ready! Press any key to send a gRPC request (or Ctrl-C to quit):");
+// 			"en-GB" => $"Good morning, {request.Name}",
+			// "en-US" => $"Howdy, {request.Name}",
+			// "ro-RO" => $"Buna dimineata, {request.Name}!",
+			// "ru-RU" => $"Доброе Утро, {request.Name}",
+			// "fr-FR" => $"Bonjour, {request.Name}",
 while (true) {
-	Console.ReadKey(true);
-	var request = new HelloRequest { Name = "CodeCamp" };
+	var key = Console.ReadKey(true);
+	var languageCode = key.KeyChar switch {
+		'0' => "en-GB",
+		'1' => "en-US",
+		'2' => "ro-RO",
+		'3' => "ru-RU",
+		'4' => "fr-FR",
+		_ => ""
+	};
+	var request = new HelloRequest { FirstName = "CodeCamp", LastName = "Bucharest", LanguageCode = languageCode };
 	var reply = await grpcClient.SayHelloAsync(request);
 	Console.WriteLine("Greeting: " + reply.Message);
 }
